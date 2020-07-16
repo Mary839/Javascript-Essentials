080eac8a779c080db835812c225fd341a3b9d8f0
Schatten <68350147+Mary839@users.noreply.github.com>
2020-07-15 21:26:44 -0600

    Add files via upload

---
+++ DAY 1/scripts/intro.js
@@ -0,0 +1,26 @@
+//alert("Click on me");
+console.log("Hey there, im  a javascript code");
+
+// for comments in JS
+//SINGLE LINE USING //
+//MULTILINE COMMENTS USING /**/
+
+/*console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");*/
+
+var a = {name: "Mary", age:"26", city: "Costa Rica"}
+var b = {name: "Safi", age:"30", city: "Belgica"}
+var c = {name: "Amaia", age:"20", city: "Australia"}
+
+
+console.time('Time taken');
+console.log("Hey");
+console.error("an error ocurrer");
+console.warn("this is a warning");
+//console.log(a, b, c);
+console.table({a, b, c});
+console.log('%c Custom message', 'color:blue;')
+console.timeEnd('Time taken');
+
---
+++ DAY 1/scripts/varletconst.js
@@ -0,0 +1,57 @@
+// VAR = CONTAINER 
+var name = "Mary" //String
+var age = 25;  // number
+var canFly = false; // boolean
+var languages = ['Español', 'English', 'German']; //array
+var friends = { 
+    name: "Vishal",
+    hobby: "Coding",
+}
+
+var a = 10;
+console.log(a);
+
+var a = null;
+console.log(a);
+
+// LET
+
+{
+
+let city;    
+let name = "Safira";
+name= "Pretty";
+console.log("Using let:", name);
+}
+console.log(name)
+
+
+
+//CONST - constante = no se puede cambiar
+const country = "Costa Rica";
+console.log(country);
+
+//country = "Uk";
+
+const frutas = ["fresa", "sandia"]
+console.log(frutas)
+frutas.push('banano');
+console.log(frutas)
+
+
+
+
+/* naming a variable
+1. no puede empezar con numero
+2. uede empezar con letras, $, _.
+3. $ comunmente usado en JQuery
+4. _ (underscore) utilizado ara declarar private variables [OOPS in Javascript]
+*/
+
+/*
+1. camelCase 
+2. kebab-case
+3.snake_case
+4.PascalCase
+*/
+
